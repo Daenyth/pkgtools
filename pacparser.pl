c4c6544b62dc63f51afa127a0619e0f263cfe098
Daenyth Blank <Daenyth+github@gmail.com>
2008-08-04 15:08:47 -0400

    pacparser: initial commit
    Purpose: parse pacman.conf and output all relevent information in bash syntax
    Signed-off-by: Daenyth Blank <Daenyth+github@gmail.com>

---
+++ pacparser.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature ':5.10'
+
+print "pacparser is a script used to parse pacman.conf and output information in a format useful for bash scripts.\n";
