92df38bb69ad2fc4b43a00a9055ed636a4a44b2f
Daenyth <Daenyth+github@gmail.com>
2009-07-19 13:28:15 -0400

    pkgfile-book.bash: Updated to use bash 4's command_not_found_handle()
    No more PROMPT_COMMAND hacks!
    
    Signed-off-by: Daenyth <Daenyth+github@gmail.com>

--- other/pkgfile-hook.bash
+++ other/pkgfile-hook.bash
@@ -1,14 +1,8 @@
 #!/bin/bash
-prompt_command () {
-	if [ $? -eq 127 ]; then
-		local _prev_command="$(history 1 | awk '{print $2}')"
-		local pkgs="$(pkgfile -b -v "$_prev_command")"
-		if [ ! -z "$pkgs" ]; then
-			echo 
-			echo "$_prev_command may be found in the following packages:"
-			echo 
-			echo -e "$pkgs"
-		fi
+command_not_found_handle () {
+	local _prev_command="$1"
+	local pkgs="$(pkgfile -b -v "$_prev_command")"
+	if [ ! -z "$pkgs" ]; then
+		echo -e "\n$_prev_command may be found in the following packages:\n$pkgs"
 	fi
 }
-PROMPT_COMMAND=prompt_command
