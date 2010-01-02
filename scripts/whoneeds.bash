045dabd3cb6b95d3148fdfd7682c3e925ab72fd6
Ryan Ouellette <ouellr@gmail.com>
2010-01-02 18:28:36 -0500

    whoneeds: Fix for infinite loop when scanning a circular dependency
    Signed-off-by: Daenyth <Daenyth+github@gmail.com>

--- scripts/whoneeds.bash
+++ scripts/whoneeds.bash
@@ -1,21 +1,51 @@
 #!/bin/bash
 # whoneeds package : shows explicitly installed packages that require some package
 
-function whoneeds_f
-{
-    direct_needs=$(LC_ALL=C pacman -Qi $1 | tr '\n' ' ' |  sed 's/.*Required By *: *//' | sed 's/Conflicts With.*//')
-    #if pacman -Qi $1 | grep -q '^Install Reason : Explicitly installed'; then
-    if [[ "$(fgrep -A1 '%REASON%' /var/lib/pacman/local/${1}-[[:digit:]]*/desc | tail -n 1)" = 1 ]]; then
-        #echo "installed expicitly"
-        echo $1
+# we use an associative array to get uniqueness for "free"
+typeset -A root_packages
+query=$1
+
+function whoneeds_f () {
+
+    local package=$1
+    
+    # If we've already found $package, we exit, else we open the 
+    # the possibility of infinitely traversing a subset of nodes
+    [[ ${root_packages[$package]} -eq 1 ]] && return 0 
+    
+    # we do this so that we can make a single call to pacman
+    # to get both bits of information that we require
+    result=( $(LC_ALL=c pacman -Qi $package | awk -F':' \
+      'BEGIN { dependents = none; explicit = 0 }
+       {
+          if ($1 ~ /^Required By/ && $2 !~ /None/) {  dependents = $2 }
+          if ($1 ~ /^Install Reason $/ && $2 ~/^ Explicitly installed/) { explicit = 1 }
+       }
+       END { print explicit,dependents}') )
+
+    if [[ ${result[0]} -eq 1 ]]; then
+        # we found an explicitly installed package that relies on
+        # the original query. Add it to our array, provided it isn't
+        # the original query, as that would be useless information
+        [[ "$query" != "$package" ]] && root_packages[$package]=1
     fi
-    if echo "$direct_needs" | grep -qv 'None'; then
-        for i in $direct_needs; do
+    if [[ "${result[1]}" != "none" ]]; then
+        # iterate over our 'Required By:' packages
+        dependents=${result[@]:1:${#result[*]}}
+        for i in $dependents; do
             whoneeds_f $i
         done
     fi
 }
 
-whoneeds_f $1 | sort -u
+whoneeds_f $1
+
+echo "Packages that depend on [$query]"
+if [[ -n "${!root_packages[@]}" ]]; then
+    echo "  ${!root_packages[@]}"
+    exit 0
+fi
+echo "  None"
+exit 1
 
 # vim: set ts=4 sw=4 et:
