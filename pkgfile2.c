1d117ef7436760d8a80efb8d4e2ff7a94aef7aae
Christopher Brannon <cmbrannon79@gmail.com>
2010-09-04 15:48:57 -0500

    Speed up pkgfile2, by replacing my_archive_readline.
    Instead of using the my_archive_readline function, we now wrap
    struct archive * in a custom I/O stream.  You can read about these
    in section 12.21.3 of the GNU C Library Reference Manual.
    We use glibc's getline to read lines of data from the stream.
    
    Signed-off-by: Christopher Brannon <cmbrannon79@gmail.com>

--- pkgfile2.c
+++ pkgfile2.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE 1
 #include <archive.h>
 #include <archive_entry.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <libgen.h>
 #include <string.h>
 #include <sys/types.h>
@@ -9,39 +11,15 @@
 #define ABUFLEN 1024
 #define FBUFLEN 10240
 
-/* 
- * Reads a line from the archive a
- * If the line is longer than FBUFLEN-1, it will be truncated
- * Returns NULL if end of file has been reached
- */
-char *my_archive_readline(struct archive *a) {
-  static char buf[FBUFLEN];
-  static char lbuf[FBUFLEN];
-  static size_t fill = 0;
-  char *n;
-  ssize_t r;
-  
-  r = archive_read_data(a, buf+fill, FBUFLEN-fill);
-  fill += r;
-  if(r<0 || fill==0) {
-    fill = 0;
-    return NULL;
-  }
+cookie_io_functions_t archive_stream_funcs = {
+.read = archive_read_data,
+.write = NULL,
+.seek = NULL,
+.close = NULL
+};
 
-  n = index(buf, '\n');
-  if(n==NULL) {
-    n = buf+FBUFLEN-1;
-    fill -= n-buf;
-  } else {
-    fill -= n-buf+1;
-  }
-  strncpy(lbuf, buf, n-buf);
-  lbuf[n-buf]='\0';
-  if(*n=='\n')
-    memmove(buf, n+1, fill);
-  else
-    memmove(buf, n, fill);
-  return lbuf;
+FILE *open_archive_stream(struct archive *archive) {
+  return fopencookie(archive, "r", archive_stream_funcs);
 }
 
 /*
@@ -66,7 +44,11 @@ void search_file(const char *repo, const char *pattern) {
   struct archive *a;
   struct archive_entry *entry;
   struct stat st;
-  char pname[ABUFLEN], *fname, *dname, *l, filename[ABUFLEN];
+  char pname[ABUFLEN], *fname, *dname, filename[ABUFLEN];
+  char *l = NULL;
+  FILE *stream = NULL;
+  size_t n = 0;
+  int nread;
   pname[ABUFLEN-1]='\0';
   strncpy(filename, repo, ABUFLEN);
   strncat(filename, ".files.tar.gz", ABUFLEN);
@@ -81,6 +63,7 @@ void search_file(const char *repo, const char *pattern) {
   archive_read_support_compression_all(a);
   archive_read_support_format_all(a);
   archive_read_open_filename(a, filename, 10240);
+  l = NULL;
   while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
     if(!S_ISREG(archive_entry_filetype(entry))) {
       archive_read_data_skip(a);
@@ -94,10 +77,26 @@ void search_file(const char *repo, const char *pattern) {
       continue;
     }
     
-    while((l = my_archive_readline(a))!=NULL)
+    stream = open_archive_stream(a);
+    if (!stream) {
+      fprintf(stderr, "Unable to open archive stream.\n");
+      exit(1);
+    }
+
+    while((nread = getline(&l, &n, stream)) != -1) {
+      /* Note: getline returns -1 on both EOF and error. */
+      /* So I'm assuming that nread > 0. */
+      if(l[nread - 1] == '\n')
+        l[nread - 1] = '\0';	/* Clobber trailing newline. */
       if(strcmp(l, "%FILES%") && match_filename(l, pattern))
         printf("%s/%s: %s\n", repo, dname, l);
+    }
+    fclose(stream);
   }
+
+  if(l)
+    free(l);
+
   archive_read_finish(a);
 }
 
