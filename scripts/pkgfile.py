4f7ce135cd942c77679e9212fb0ca98f4e354d9e
Daenyth <Daenyth+github@gmail.com>
2011-01-16 13:46:27 -0500

    pkgfile.py: Fix bug when used with pacman-git
    calling pacman with no action arguments no longer prints the debug info
    we need. Call it with -T to effectively noop and get it

--- scripts/pkgfile.py
+++ scripts/pkgfile.py
@@ -42,7 +42,7 @@ FILELIST_DIR = '/var/cache/pkgtools/lists'
 def find_dbpath():
     '''find pacman dbpath'''
 
-    p = subprocess.Popen(['pacman', '-v'], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['pacman', '-Tv'], stdout=subprocess.PIPE)
     output = p.communicate()[0]
     for line in output.split('\n'):
         if line.startswith('DB Path'):
@@ -149,7 +149,7 @@ def update_repo(options, target_repo=None):
     if not os.access(FILELIST_DIR, os.F_OK|os.R_OK|os.W_OK|os.X_OK):
         die(1, 'Error: %s is not accessible' % FILELIST_DIR)
 
-    p = subprocess.Popen(['pacman', '--debug'], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['pacman', '-T', '--debug'], stdout=subprocess.PIPE)
     output = p.communicate()[0]
 
     # get a list of repo and mirror
