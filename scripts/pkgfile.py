755e147b3a660c0f5fdbed7df9bd88b3ffdf874e
Daenyth <Daenyth+github@gmail.com>
2011-01-16 12:42:43 -0500

    pkgfile.py: Don't print directories with -bl

--- scripts/pkgfile.py
+++ scripts/pkgfile.py
@@ -286,7 +286,7 @@ def list_files(s, options):
             for f in sorted(m['files']):
                 if options.binaries:
                     # XXX: Duplicated code?
-                    if 'bin/' in f:
+                    if 'bin/' in f and not f.endswith('/'):
                         print '%s /%s' % (m['name'], f)
                         foundpkg = True
                 else:
